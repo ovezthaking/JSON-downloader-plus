0a4aeca3f912f6ac44e1a0e6fd1e360ab4c3d3b0
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-12 01:39:46 +0100

    Popraw błędy w funkcji get_post i dodaj funkcję save_json do zapisywania postów w pliku JSON

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import requests
+import json
+
+
+url = 'htstps://jsonplaceholder.typicode.com/posts'
 
 
 def get_post(index):
     if type(index) is not int:
         raise TypeError('Index must be int')
 
-    url = f'htstps://jsonplaceholder.typicode.com/posts/{index}'
     try:
-        response = requests.get(url)
+        response = requests.get(f'{url}/{index}')
     except Exception as e:
         return f'Error: {e}'
     else:
         return response.json()
 
 
-def create_post(title, body):
+def save_json(title='', body=''):
+    title = input('Type the title')
+    body = input('Type the content')
+
+    payload = {"title": title, "body": body, "userId": 1}
+
+    try:
+        file = open('post.json', 'w')
+        file.write(json.dumps(payload, indent=2))
+    except Exception as e:
+        return f'Error: {e}'
+    finally:
+        file.close()
+
+
+def create_post(u_file):
+    
     requests.post()
 
 
 print(get_post(2))
+
+save_json()
