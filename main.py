766a37ecfcc07bf28cc81cd5cdc98ff951fdadd3
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-12 02:38:42 +0100

    Dodaj funkcję logowania do funkcji oraz popraw błędy w obsłudze wyjątków w funkcjach upload_post, read_file i save_json

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ import time
 
 
 url = 'https://jsonplaceholder.typicode.com/posts'
+now = str(time.time())[:12].replace('.', '')
 
 
+def logs(func):
+    def wrapper(*args, **kwargs):
+        with open('logs.txt', 'a') as file:
+            file.write(f'\n--{now}-- {func.__name__}')
+            result = func(*args, **kwargs)
+            file.write(f'\n{result}')
+            file.write('\n--finished--')
+        return result
+    return wrapper
+
+
+@logs
 def get_post(index):
     if type(index) is not int:
         raise TypeError('Index must be int')
@@ -18,6 +31,7 @@ def get_post(index):
         return response.json()
 
 
+@logs
 def save_file(filename, title='', body=''):
     if not title and not body:
         title = input('Type the title')
@@ -27,7 +41,6 @@ def save_file(filename, title='', body=''):
     elif not body and title:
         body = input('Type the content')
 
-    now = str(time.time())[:12].replace('.', '')
     payload = {"title": title, "body": body, "userId": 1}
 
     try:
@@ -39,6 +52,7 @@ def save_file(filename, title='', body=''):
         file.close()
 
 
+@logs
 def save_json(filename, get_payload):
 
     now = str(time.time())[:12].replace('.', '')
@@ -48,44 +62,53 @@ def save_json(filename, get_payload):
         file = open(f'{filename}_{now}.json', 'w')
         file.write(json.dumps(payload, indent=2))
     except Exception as e:
-        raise f'Error: {e}'
+        return RuntimeError(f'Error: {e}')
     finally:
         file.close()
 
 
+@logs
 def read_file(filename):
     try:
         file = open(filename, 'r')
         content = json.load(file)
     except Exception as e:
-        raise f'Error: {e}'
+        return f'Error: {e}'
     finally:
         file.close()
 
     return content
 
 
+@logs
 def upload_post(filename):
     try:
-        file = open(filename, 'r')
-        payload = json.load(file)
+        with open(filename, 'r') as file:
+            payload = json.load(file)
     except Exception as e:
-        raise f'Error: {e}'
-    finally:
-        file.close()
+        raise RuntimeError(f'Error: {e}')
 
     try:
-        res = requests.post(url, json=payload)
+        res = requests.post(url, json=payload, timeout=10)
     except Exception as e:
-        raise f'Error:{e}'
+        return RuntimeError(f'Error: {e}')
 
-    if not res.status_code >= 200 and not res.status_code < 300:
-        raise f'Error status code: {res.status_code}'
+    if not (200 <= res.status_code < 300):
+        return RuntimeError(f'Error status code: '
+                            f'{res.status_code}, body: {res.text}')
     return res
+
+
+def search_log(keyword):
+    with open('logs.txt', 'r') as f:
+        for line in f:
+            if keyword in line:
+                print(line)
 
 
 if __name__ == '__main__':
     # print(get_post(2))
     # save_json('jsonplaceh', get_post(3))
-    # print(read_file('jsonplaceh_17655012183.json'))
+    print(read_file('jsonplaceh_17655012183.json'))
     print(upload_post('post.json'))
+    search_log('Error')
